2d6b039fee21e86b74efc79640463829f5e889d2
Haziel Aguilar <alfalex64x@gmail.com>
2023-11-23 21:50:56 -0600

    Seccion "Statement" terminado

--- ASDR.java
+++ ASDR.java
@@ -42,13 +42,17 @@ public class ASDR implements Parser
     private void DECLARATION()
     {
         if(hayErrores) return;
-
     }
 
     //FUN_DECL -> fun FUNCTION
     private void FUN_DECL()
     {
         if(hayErrores) return;
+        if(preanalisis.tipo==TipoToken.FUN)
+        {
+            match(TipoToken.FUN);
+            FUNCTION();
+        }
     }
 
     //FUNCTION -> id ( PARAMETERS_OPC ) BLOCK
@@ -86,6 +90,13 @@ public class ASDR implements Parser
     private void VAR_DECL()
     {
         if(hayErrores) return;
+        if(preanalisis.tipo==TipoToken.VAR)
+        {
+            match(TipoToken.VAR);
+            match(TipoToken.IDENTIFIER);
+            VAR_INIT();
+            match(TipoToken.SEMICOLON);
+        }
     }
 
     /*
@@ -95,6 +106,15 @@ public class ASDR implements Parser
     private void VAR_INIT()
     {
         if(hayErrores) return;
+        if(preanalisis.tipo==TipoToken.EQUAL)
+        {
+            match(TipoToken.EQUAL);
+            EXPRESSION();
+        }
+        else
+        {
+            //Cadena vacia
+        }
     }
 
     //EXPRESSION -> ASSIGNMENT
@@ -284,18 +304,44 @@ public class ASDR implements Parser
     private void STATEMENT()
     {
         if(hayErrores) return;
+        if(preanalisis.isExpression())
+            EXPR_STMT();
+        if(preanalisis.tipo==TipoToken.FOR)
+            FOR_STMT();
+        if(preanalisis.tipo==TipoToken.IF)
+            IF_STMT();
+        if(preanalisis.tipo==TipoToken.PRINT)
+            PRINT_STMT();
+        if(preanalisis.tipo==TipoToken.RETURN)
+            RETURN_STMT();
+        if(preanalisis.tipo==TipoToken.WHILE)
+            WHILE_STMT();
+        if(preanalisis.tipo==TipoToken.LEFT_BRACE)
+            BLOCK();
     }
 
     //EXPR_STMT -> EXPRESSION ;
     private void EXPR_STMT()
     {
         if(hayErrores) return;
+        EXPRESSION();
+        match(TipoToken.SEMICOLON);
     }
 
     //FOR_STMT -> for ( FOR_STMT_1 FOR_STMT_2 FOR_STMT_3 ) STATEMENT
     private void FOR_STMT()
     {
         if(hayErrores) return;
+        if(preanalisis.tipo==TipoToken.FOR)
+        {
+            match(TipoToken.FOR);
+            match(TipoToken.LEFT_PAREN);
+            FOR_STMT_1();
+            FOR_STMT_2();
+            FOR_STMT_3();
+            match(TipoToken.RIGHT_PAREN);
+            STATEMENT();
+        }
     }
 
     /*
@@ -306,6 +352,12 @@ public class ASDR implements Parser
     private void FOR_STMT_1()
     {
         if(hayErrores) return;
+        if(preanalisis.tipo==TipoToken.VAR)
+            VAR_DECL();
+        else if(preanalisis.isExpression())
+            EXPR_STMT();
+        else if(preanalisis.tipo==TipoToken.SEMICOLON)
+            match(TipoToken.SEMICOLON);
     }
 
     /*
@@ -315,6 +367,13 @@ public class ASDR implements Parser
     private void FOR_STMT_2()
     {
         if(hayErrores) return;
+        if(preanalisis.isExpression())
+        {
+            EXPRESSION();
+            match(TipoToken.SEMICOLON);
+        }
+        else
+            match(TipoToken.SEMICOLON);
     }
 
     /*
@@ -324,12 +383,23 @@ public class ASDR implements Parser
     private void FOR_STMT_3()
     {
         if(hayErrores) return;
+        if(preanalisis.isExpression())
+            EXPRESSION();
     }
 
     //IF_STMT -> if ( EXPRESSION ) STATEMENT ELSE_STATEMENT
     private void IF_STMT()
     {
         if(hayErrores) return;
+        if(preanalisis.tipo==TipoToken.IF)
+        {
+            match(TipoToken.IF);
+            match(TipoToken.LEFT_PAREN);
+            EXPRESSION();
+            match(TipoToken.RIGHT_PAREN);
+            STATEMENT();
+            ELSE_STATEMENT();
+        }
     }
 
     /*
@@ -339,18 +409,35 @@ public class ASDR implements Parser
     private void ELSE_STATEMENT()
     {
         if(hayErrores) return;
+        if(preanalisis.tipo==TipoToken.ELSE)
+        {
+            match(TipoToken.ELSE);
+            STATEMENT();
+        }
     }
 
     //PRINT_STMT -> print EXPRESSION ;
     private void PRINT_STMT()
     {
         if(hayErrores) return;
+        if(preanalisis.tipo==TipoToken.PRINT)
+        {
+            match(TipoToken.PRINT);
+            EXPRESSION();
+            match(TipoToken.SEMICOLON);
+        }
     }
 
     //RETURN_STMT -> return RETURN_EXP_OPC ;
     private void RETURN_STMT()
     {
         if(hayErrores) return;
+        if(preanalisis.tipo==TipoToken.RETURN)
+        {
+            match(TipoToken.RETURN);
+            RETURN_EXP_OPC();
+            match(TipoToken.SEMICOLON);
+        }
     }
 
     /*
@@ -360,20 +447,38 @@ public class ASDR implements Parser
     private void RETURN_EXP_OPC()
     {
         if(hayErrores) return;
+        if(preanalisis.isExpression())
+        {
+            EXPRESSION();
+        }
     }
 
     //WHILE_STMT -> while ( EXPRESSION ) STATEMENT
     private void WHILE_STMT()
     {
         if(hayErrores) return;
+        if(preanalisis.tipo==TipoToken.WHILE)
+        {
+            match(TipoToken.WHILE);
+            match(TipoToken.LEFT_PAREN);
+            EXPRESSION();
+            match(TipoToken.RIGHT_PAREN);
+            STATEMENT();
+        }
     }
 
     //BLOCK -> { DECLARATION }
     private void BLOCK()
     {
         if(hayErrores) return;
+        if(preanalisis.tipo==TipoToken.LEFT_BRACE)
+        {
+            match(TipoToken.LEFT_BRACE);
+            DECLARATION();
+            match(TipoToken.RIGHT_BRACE);
+        }
     }
-    
+
     /******************************************************************************************************/
     private void match(TipoToken tipoToken)
     {
