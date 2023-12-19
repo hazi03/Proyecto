fddb31a70ebcccff9cdd724210d819f8b396683e
Haziel Aguilar <alfalex64x@gmail.com>
2023-12-19 06:29:28 -0600

    Añadidas funciones auxiliares para el AST

--- Token.java
+++ Token.java
@@ -19,6 +19,7 @@ public class Token {
         this.posicion = 0;
     }*/
 
+    /* Métodos auxiliares para el AST */
     public boolean isExpression()
     {
         switch(this.tipo)
@@ -34,6 +35,122 @@ public class Token {
                 return false;
         }
     }
+
+    public boolean isOperator()
+    {
+        switch(this.tipo)
+        {
+            case PLUS:
+            case MINUS:
+            case STAR:
+            case SLASH:
+            case EQUAL:
+            case EQUAL_EQUAL:
+            case LESS:
+            case LESS_EQUAL:
+            case GREATER:
+            case GREATER_EQUAL:
+            case BANG:
+            case BANG_EQUAL:
+            case OR:
+            case AND:
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    public boolean isKeyword()
+    {
+        switch(this.tipo)
+        {
+            case PRINT:
+            case RETURN:
+            case VAR:
+            case NULL:
+            case FUN:
+            case IF:
+            case ELSE:
+            case WHILE:
+            case FOR:
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    public boolean isControlStructure()
+    {
+        switch(this.tipo)
+        {
+            case IF:
+            case ELSE:
+            case WHILE:
+            case FOR:
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    public boolean precedenceGreaterEqual(Token token)
+    {
+        return this.getPrecedence() >= token.getPrecedence();
+    }
+
+    private int getPrecedence()
+    {
+        switch(this.tipo)
+        {
+            case PLUS:
+            case MINUS:
+                return 6;
+            case STAR:
+            case SLASH:
+                return 7;
+            case LESS:
+            case LESS_EQUAL:
+            case GREATER:
+            case GREATER_EQUAL:
+                return 5;
+            case EQUAL_EQUAL:
+                return 4;
+            case EQUAL:
+                return 1;
+            case OR:
+                return 2;
+            case AND:
+                return 3;
+            default:
+                return 0;
+        }
+    }
+
+    public int arity()
+    {
+        switch(this.tipo)
+        {
+            case PLUS:
+            case MINUS:
+            case STAR:
+            case SLASH:
+            case EQUAL:
+            case EQUAL_EQUAL:
+            case LESS:
+            case LESS_EQUAL:
+            case GREATER:
+            case GREATER_EQUAL:
+            case BANG:
+            case BANG_EQUAL:
+            case OR:
+            case AND:
+                return 2;
+            default:
+                return 0;
+        }
+    }
+
+    /*******************************************************************************************************/
 
     @Override
     public boolean equals(Object o)
